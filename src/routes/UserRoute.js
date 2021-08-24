a1f9a6b9f20231f333855231c7ef161d5b3509c9
thatsickusername <89441376+man1shpandey@users.noreply.github.com>
2021-08-24 13:42:46 +0530

    local storage checking

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -4,6 +4,7 @@ import { AuthContext } from '../context/auth';
 
 const UserRoute = ({component:Component, ...rest}) => {
     const { user } = useContext(AuthContext);
+    console.log(user)
     return (
         <Route {...rest} render={props => user ? <Component {...props}/> : <Redirect to="/"/>}/>
     );
